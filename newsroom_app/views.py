4d0bbd555edd9a7c24b838771400d58e13849aa8
yahayakenny <y.hkehinde@yahoo.com>
2020-08-10 09:38:28 +0100

    saved user info to dataabase

--- newsroom_app/views.py
+++ newsroom_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User, auth
 
 
 # Create your views here.
@@ -12,4 +13,18 @@ def movies(request):
     return render(request, 'movies.html')
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        user = User.objects.create_user(username = username, password = password1, email = email, first_name = first_name, last_name = last_name)
+        user.save()
+        note = 'Thank you ' + first_name + ' for signing up with us'
+        return render(request, 'register.html', {'note': note})
+
+    else:
+        return render(request, 'register.html')
